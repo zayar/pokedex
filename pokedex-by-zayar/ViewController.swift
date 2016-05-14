bb760eca05737c6dde733fb430cf9207bb4b9c6c
Zayar Min <zayarmin@gmail.com>
2016-05-14 22:20:07 +0800

    Initial Commit

---
+++ pokedex-by-zayar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-zayar
+//
+//  Created by zayar min on 14/5/16.
+//  Copyright © 2016 zayarmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
